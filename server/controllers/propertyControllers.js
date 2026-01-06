3134309e70cad5a1fab277394cc17496392126f0
ogiwow1 <35508451+ogiwow1@users.noreply.github.com>
2026-01-07 00:06:48 +0100

    Update propertyControllers.js

--- server/controllers/propertyControllers.js
+++ server/controllers/propertyControllers.js
@@ -1,1 +1,19 @@
+exports.getPropertiesInView = async (req, res) => {
+  const { sw_lat, sw_lng, ne_lat, ne_lng } = req.query;
 
+  try {
+    const homes = await Property.find({
+      location: {
+        $geoWithin: {
+          $box: [
+            [parseFloat(sw_lng), parseFloat(sw_lat)], // Bottom-left corner
+            [parseFloat(ne_lng), parseFloat(ne_lat)]  // Top-right corner
+          ]
+        }
+      }
+    });
+    res.json(homes);
+  } catch (err) {
+    res.status(500).json({ error: "Spatial search failed" });
+  }
+};
