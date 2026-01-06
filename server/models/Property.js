18b3df81c26b1265be7833b17973f466644d5ee5
ogiwow1 <35508451+ogiwow1@users.noreply.github.com>
2026-01-06 23:42:56 +0100

    Update Property.js

--- server/models/Property.js
+++ server/models/Property.js
@@ -1,1 +1,19 @@
+const mongoose = require('mongoose');
 
+const PropertySchema = new mongoose.Schema({
+  title: String,
+  price: Number,
+  address: String,
+  beds: Number,
+  baths: Number,
+  sqft: Number,
+  images: [String],
+  coordinates: {
+    lat: Number,
+    lng: Number
+  },
+  zestimate: Number,
+  type: { type: String, enum: ['For Sale', 'For Rent', 'Sold'] }
+});
+
+module.exports = mongoose.model('Property', PropertySchema);
