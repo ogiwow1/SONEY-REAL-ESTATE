99a3371bcfa02262205bf95e3d1bc5f2e87eceae
ogiwow1 <35508451+ogiwow1@users.noreply.github.com>
2026-01-07 00:18:44 +0100

    Update api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,1 +1,17 @@
+import { useMapEvents } from 'react-leaflet';
 
+function MapScout({ setProperties }) {
+  const map = useMapEvents({
+    moveend: () => {
+      const bounds = map.getBounds();
+      const sw = bounds.getSouthWest();
+      const ne = bounds.getNorthEast();
+
+      // Fetch homes in this new window
+      fetch(`/api/properties/search?sw_lat=${sw.lat}&sw_lng=${sw.lng}&ne_lat=${ne.lat}&ne_lng=${ne.lng}`)
+        .then(res => res.json())
+        .then(data => setProperties(data));
+    },
+  });
+  return null;
+}
